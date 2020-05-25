9a1931b61ee43b0eab25f9ddaa841bcc69c722e8
brunobc <brunobezerrachaves@gmail.com>
2020-05-25 10:08:02 -0300

    List products from api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,107 +1,46 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { MdAddShoppingCart } from 'react-icons/md'
+
+import { formatPrice } from '../../util/format'
+import api from '../../services/api'
 
 import { ProductList } from './styles'
 
 export default function Home() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    async function getProducts() {
+      try {
+        const response = await api.get('products')
+        const data = response.data.map((product) => ({
+          ...product,
+          priceFormatted: formatPrice(product.price),
+        }))
+        setProducts(data)
+      } catch (e) {
+        console.log(e)
+      }
+    }
+    getProducts()
+  }, [])
   return (
     <ProductList>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice titulo muito grande para quebra de linha</strong>
-        <span>R$129,90</span>
+      {products.map((product) => (
+        <li key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <strong>{product.title}</strong>
+          <span>{product.priceFormatted}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart siz={16} color="#FFF" /> 5
+            </div>
 
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
-
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
-
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
-
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
-
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img
-          src="https://static.netshoes.com.br/produtos/tenis-adidas-ultraboost-20-masculino/06/NQQ-3290-006/NQQ-3290-006_zoom2.jpg?ts=1587073224&ims=326x"
-          alt="Tênis"
-        />
-        <strong>Tênis nice</strong>
-        <span>R$129,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart siz={16} color="#FFF" /> 5
-          </div>
-
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+            <span>Adicionar ao carrinho</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   )
 }
