d0a7e8749fbce1250f4c0885fac93b92ef209119
brunobc <brunobezerrachaves@gmail.com>
2021-03-23 14:30:58 -0300

    remove server folder

--- index.js
+++ index.js
@@ -9,16 +9,15 @@ const middlewares = jsonServer.defaults()
 const PORT = process.env.PORT || 5000;
 
 app.use(middlewares);
+app.use(router)
 
-if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')));
 
   app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-}
+  });
 
-app.use(router)
+
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
